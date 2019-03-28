a0be4e779875e7291be01102bbd4460febaad43c
alancarvalhobrito <alan_carvalhobrito@hotmail.com>
2019-03-28 06:38:44 -0300

    Ajuste no Path

--- src/main/java/com/backend/projeto/controller/TelefoneController.java
+++ src/main/java/com/backend/projeto/controller/TelefoneController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/telefones")
+@RequestMapping("api/telefones")
 public class TelefoneController {
 
     @Autowired
